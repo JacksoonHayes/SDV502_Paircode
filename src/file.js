318b2335b0c067e5db414ae112d1c604f00b7590
JacksoonHayes <jackson-hayes@live.nmit.ac.nz>
2023-05-10 09:51:28 +1200

    another version of the container function function

--- src/file.js
+++ src/file.js
@@ -1,1 +1,10 @@
-console.log('Hi Jackson!');
+// Function to calculate the volume of a container
+function volumeOfContainer(container) {
+    container = {
+        width: 5,
+        length: 5,
+        height: 5
+    }
+    return container.width * container.length * container.height;
+}
+console.log(volumeOfContainer(container));
