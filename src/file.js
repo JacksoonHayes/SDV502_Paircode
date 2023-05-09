724d2b174aae1d17ffae5796787ce530059c5fd9
JacksoonHayes <jackson-hayes@live.nmit.ac.nz>
2023-05-10 10:01:46 +1200

    function to utilise th object.entries method

--- src/file.js
+++ src/file.js
@@ -1,10 +1,21 @@
 // Function to calculate the volume of a container
-function volumeOfContainer(container) {
-    container = {
-        width: 5,
-        length: 5,
-        height: 5
-    }
-    return container.width * container.length * container.height;
+// function volumeOfContainer() {
+//     container = {
+//         width: 5,
+//         length: 5,
+//         height: 5
+//     }
+//     return container.width * container.length * container.height;
+// }
+// console.log(volumeOfContainer());
+
+//Function to convert all properties in an object into an array
+object = {
+    name: "John",
+    age: 30,
+    city: "New York"
 }
-console.log(volumeOfContainer());
+function convertObjectToArray() {
+    return Object.entries(object);
+}
+console.log(convertObjectToArray());
