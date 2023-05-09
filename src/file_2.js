92db6fb461c7eeb6f247844b7494941ee76b7c9a
Jayden Houghton <109559841+Jayden-Htn@users.noreply.github.com>
2023-05-10 09:46:15 +1200

    build nested properties version

--- src/file_2.js
+++ src/file_2.js
@@ -1,9 +1,23 @@
-// Jayden's version
+// Jayden's versions
 
+
+// original volume calculation
 container = {width: 5, height: 5, length: 5};
 
 function calcVolume(cont) {
     return cont.width * cont.height * cont.length;
 }
 
-console.log(calcVolume(container));
+// console.log(calcVolume(container));
+
+
+// nested properties version
+container = {
+    dimensions: {width: 5, height: 5, length: 5}
+};
+
+function calcVolume2(dim) {
+    return dim.width * dim.height * dim.length;
+}
+
+console.log(calcVolume2(container.dimensions));
