f12e0fd24094414c4aa1d0278c7351c85cdcbc07
Jayden Houghton <109559841+Jayden-Htn@users.noreply.github.com>
2023-05-10 09:41:20 +1200

    write volume calculation function

---
+++ src/file_2.js
@@ -0,0 +1,9 @@
+// Jayden's version
+
+container = {width: 5, height: 5, length: 5};
+
+function calcVolume(cont) {
+    return cont.width * cont.height * cont.length;
+}
+
+console.log(calcVolume(container));
